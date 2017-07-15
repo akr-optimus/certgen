66e66b238ad856e8f49a49a6e64aff1ef4c76e97
Abhisek <abhisekkumar_rout@mcafee.com>
2017-07-15 23:24:09 +0530

    Process only if openssl is available

--- certgen.sh
+++ certgen.sh
@@ -3,20 +3,20 @@
 certmeta=`pwd`/certmeta
 
 OPENSSL=`which openssl`
-COMMON_NAME=`cat ${certmeta} | grep "CN" | cut -d= -f2 | tr -d [:space:]`
-if [[ ! -z $1 && $1 == '--read' ]]; then
-    if [[ -f $COMMON_NAME.crt ]]; then
-        $OPENSSL x509 -in $COMMON_NAME.crt -text -noout
-        exit 0
-    else
-        echo "$COMMON_NAME.crt not found"
-        exit 1
-    fi
-fi
 if [[ -z "$OPENSSL" ]]; then
     echo "openssl not found"
     exit 1;
 else
-    SERVER_KEY=`$OPENSSL req  -nodes -new -x509  -keyout ${COMMON_NAME}.key -out ${COMMON_NAME}.crt -days 365 -config ${certmeta}`
-    echo $SERVER_KEY
+    COMMON_NAME=`cat ${certmeta} | grep "CN" | cut -d= -f2 | tr -d [:space:]`
+    if [[ ! -z $1 && $1 == '--read' ]]; then
+        if [[ -f $COMMON_NAME.crt ]]; then
+            $OPENSSL x509 -in $COMMON_NAME.crt -text -noout
+            exit 0
+        else
+            echo "$COMMON_NAME.crt not found"
+            exit 1
+        fi
+    fi
+    CERT_GEN=`$OPENSSL req  -nodes -new -x509  -keyout ${COMMON_NAME}.key -out ${COMMON_NAME}.crt -days 365 -config ${certmeta}`
+    echo $CERT_GEN
 fi
